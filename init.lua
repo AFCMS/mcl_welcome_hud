98d482ca82c6491feca15a90fbea1ad678d1d98a
AFCMS <afcm.contact@gmail.com>
2021-04-12 18:42:02 +0200

    add timeout setting

--- init.lua
+++ init.lua
@@ -9,15 +9,11 @@ local ids = {}
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
     ids[name] = mcl_bossbars.add_bar(player, {text=params.text, color=params.color, percentage=100, dynamic=false})
-    --if params.timeout ~= 0 then
-    --    minetest.after(params.timeout, function(name)
-    --        local player = minetest.get_player_by_name(name)
-    --        --minetest.chat_send_all(ids[name])
-    --        if player and ids[name] then
-    --            mcl_bossbars.remove_bar(ids[name])
-    --        end
-    --    end, name)
-    --end
+    if params.timeout ~= 0 then
+        minetest.after(params.timeout, function()
+            mcl_bossbars.remove_bar(ids[name])
+        end)
+    end
 end)
 
 minetest.register_on_leaveplayer(function(player)
