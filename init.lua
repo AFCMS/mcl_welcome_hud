ebf292475c96ff953cef040a0eed33fc1d43dfe2
AFCMS <61794590+AFCMS@users.noreply.github.com>
2021-05-24 10:05:05 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local params = {}
-params.text = minetest.settings:get("mcl_welcome_hud_msg") or "WELCOME"
-params.color = minetest.settings:get("mcl_welcome_hud_color") or "white"
-params.timeout = minetest.settings:get("mcl_welcome_hud_timeout")
+params.text = minetest.settings:get("mcl_welcome_hud.msg") or "WELCOME"
+params.color = minetest.settings:get("mcl_welcome_hud.color") or "white"
+params.timeout = minetest.settings:get("mcl_welcome_hud.timeout")
 
 minetest.register_on_joinplayer(function(player)
     mcl_bossbars.add_bar(player, {text=params.text, color=params.color, percentage=100, dynamic=false, timeout=params.timeout})
